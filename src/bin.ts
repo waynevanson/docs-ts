238f792e657ee5bd46fd7eeb23fc73f539382a3f
Wayne Van Son <waynevanson@gmail.com>
2021-02-21 11:18:24 +1100

    feat: print error as non coerced string

--- src/bin.ts
+++ src/bin.ts
@@ -6,9 +6,7 @@
  * @since 0.2.0
  */
 
-import chalk from 'chalk'
-
 import { main } from '.'
 
 // tslint:disable-next-line: no-console
-main().catch((e) => console.log(chalk.bold.red(`Unexpected error: ${e}`)))
+main().catch((e) => console.error(e))
